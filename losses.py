dcc9c490fceab80e365f0e12d315f9b04f478abb
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2022-02-09 21:33:31 -0600

    New loss function added

--- losses.py
+++ losses.py
@@ -5,3 +5,9 @@ def mse(y_predicted, y_true):
 
 def d_mse(y_predicted, y_true):
     return 2 * (y_predicted - y_true) / np.size(y_true)
+
+def cross_entropy(y_predicted, y_true):
+    return -y_true * np.log(y_predicted) - (1 - y_true) * np.log(1 - y_predicted)
+
+def d_cross_entropy(y_predicted, y_true):
+    return -(y_true / y_predicted) + (1 - y_true) / (1 - y_predicted)
